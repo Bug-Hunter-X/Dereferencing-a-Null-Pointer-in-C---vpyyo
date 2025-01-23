d98c0437d6564211da30307389d56ffe7254bace
BugHunter-X <askzenai@gmail.com>
2025-01-23 16:58:08 +0100

    Add bugSolution.cpp file

---
+++ bugSolution.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int* ptr = nullptr;
+  if (ptr != nullptr) {
+    *ptr = 10; // Dereferencing only if ptr is not null
+  } else {
+    std::cerr << "Error: Pointer is null" << std::endl;
+  }
+  return 0;
+}
